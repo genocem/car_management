226ee907c460ea8193d82f67771e420080043f0a
genocem <ahmedkacemkacem2@gmail.com>
2024-07-30 12:39:00 +0100

    reorganised the database.js file

--- components/Database.js
+++ components/Database.js
@@ -2,6 +2,8 @@ import * as SQLite from 'expo-sqlite';
 
 const db = SQLite.openDatabaseAsync('carDB');
 
+// functions to manipulate the car table
+
 export const getVoitures = async (setEntries) => {
     const fetchedEntries = [];
     for await (const row of (await db).getEachAsync('SELECT * FROM voiture')) {
@@ -10,7 +12,6 @@ export const getVoitures = async (setEntries) => {
             nomProprietere: row.nomProprietere,
             kilometrageTotale: row.kilometrageTotale
         });
-        // console.log(row);
     }
     setEntries(fetchedEntries);
 }
@@ -27,28 +28,9 @@ export const AddCarDB = async (nomProprietere, matricule, kilometrageTotale) => 
     (await db).runAsync('INSERT INTO voiture (nomProprietere, matricule, kilometrageTotale) VALUES (?, ?, ?)', [nomProprietere, matricule, kilometrageTotale]);
 
 };
+// functions to manipulate the rest of the tables
 
-// here the function that will delete the entry based on the type of the entry
-export const deleteEntry = async (matricule, table) => {
-    try {
-        (await db).runAsync('DELETE FROM ' + table + ' WHERE matricule = ? ' , [matricule]);
-    } catch (error) {
-        console.error("Failed to delete entry:", error);
-    }
-}
 
-// export const getTable = async (setEntries, Tname, matricule) => {
-//     const fetchedEntries = [];
-//     for await (const row of (await db).getEachAsync('SELECT * FROM ? WHERE matricule = ?', [Tname, matricule])) {
-//         // diffrent table cannot have the same column name so the .push will just get the first 2 columns of a row
-//         fetchedEntries.push({
-//             id: row.id,
-//             name: row.name,
-//         });
-//         console.log(row);
-//     }
-//     setEntries(fetchedEntries);
-// }
 export const getTable = async (setEntries, Tname, matricule) => {
     const fetchedEntries = [];
     try {
@@ -60,7 +42,6 @@ export const getTable = async (setEntries, Tname, matricule) => {
                     id: row.id,
                     name: row.date,
                 });
-
             }
             else {
                 fetchedEntries.push({
@@ -77,6 +58,13 @@ export const getTable = async (setEntries, Tname, matricule) => {
     }
 }
 
+export const getKillometrage = async (matricule, setKilometrageOld) => {
+    // there is only ine row with the matricule
+    for await (const row of (await db).getEachAsync('SELECT kilometrageTotale FROM voiture WHERE matricule = ?', [matricule])) {
+        setKilometrageOld(row.kilometrageTotale);
+    }
+}
+
 export const deleteTableEntry = async (Tname, id) => {
     try {
         (await db).runAsync('DELETE FROM ' + Tname + ' WHERE id = ? ', [id]);
@@ -85,21 +73,15 @@ export const deleteTableEntry = async (Tname, id) => {
     }
 }
 
+
 export const AddAssurance = async (matricule, nom, dateAssurance, dureeEnMois, prixAssurance) => {
     (await db).runAsync('INSERT INTO assurance (matricule, nom, dateAssurance, dureeEnMois, prixAssurance) VALUES (?, ?, ?, ?, ?)', [matricule, nom, dateAssurance, dureeEnMois, prixAssurance]);
 }
 
-export const getKillometrage = async (matricule, setKilometrageOld) => {
-    // there is only ine row with the matricule
-    for await (const row of (await db).getEachAsync('SELECT kilometrageTotale FROM voiture WHERE matricule = ?', [matricule])) {
-        setKilometrageOld(row.kilometrageTotale);
-    }
-}
-
-
 export const AddEntretienKilometre = async (matricule, nom, kilometrageOld, limiteKilometre) => {
     (await db).runAsync('INSERT INTO entretienKilometre (matricule, nom, kilometrageOld, limiteKilometre) VALUES (?, ?, ?, ?)', [matricule, nom, kilometrageOld, limiteKilometre]);
 }
+
 export const AddEntretienDate = async (matricule, nom, date1, date2) => {
     await (await db).runAsync(
         'INSERT INTO entretienDate (matricule, nom, date1, date2) VALUES (?, ?, ?, ?)',
@@ -110,36 +92,16 @@ export const AddEntretienDate = async (matricule, nom, date1, date2) => {
 export const AddConsommationGazoile = async (quantiteCarburant, date, kilometrage, matricule, prix) => {
     (await db).runAsync('INSERT INTO consommationGazoile (quantiteCarburant, date, kilometrage, matricule, prix) VALUES (?, ?, ?, ?, ?)', [quantiteCarburant, date, kilometrage, matricule, prix]);
 }
-export const AddKilometrage = async (matricule, date, kilometrageOld, kilometrage) =>{
+
+export const AddKilometrage = async (matricule, date, kilometrageOld, kilometrage) => {
     (await db).runAsync('INSERT INTO kilometrage (matricule, date, kilometrageOld, kilometrageAjouter) VALUES (?, ?, ?, ?)', [matricule, date, kilometrageOld, kilometrage]);
 }
 
-// export const getTables = async (setEntries) => {
-//     const fetchedEntries = [];
-//     for await (const row of (await db).getEachAsync('SELECT name FROM sqlite_master WHERE type="table" and name not like "voiture"')) {
-//         // SELECT name FROM sqlite_master WHERE type='table';
-
-//         fetchedEntries.push({
-//             name: row.name
-//         });
-//         console.log(row);
-//     }
-//     setEntries(fetchedEntries);
-// }
 
 
+// the next part here is used to manage and see database contents 
+//cause it is hard to get the sqlite file and modify it using the emulator 
 
-
-// export const smth = async () => {
-//     try {
-//         (await db).runAsync('alter table voiture drop column dateMiseEnCirculation');
-//     } catch (error) {
-
-//     }    ALTER TABLE assurance RENAME COLUMN voitureMatricule to matricule; 
-//ALTER TABLE consommationGazoile RENAME COLUMN voitureMatricule to matricule;
-// }
-// alter table entretienDate matricule text;
-// alter talbe entretienKilometre matricule text;
 export const script = async () => {
     try {
         (await db).execAsync(`
@@ -169,7 +131,6 @@ export const getColumns = async () => {
     const fetchedEntries = [];
     console.log('taburu  \n');
     for await (const row of (await db).getEachAsync('PRAGMA table_info(kilometrage)')) {
-        // SELECT name FROM sqlite_master WHERE type='table';
 
         fetchedEntries.push({
             name: row.name
@@ -181,7 +142,6 @@ export const getTableContents = async () => {
     const fetchedEntries = [];
     console.log('taburu  \n');
     for await (const row of (await db).getEachAsync('SELECT * FROM entretienKilometre ')) {
-        // SELECT name FROM sqlite_master WHERE type='table';
 
         fetchedEntries.push({
             name: row.name
